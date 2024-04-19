08ff58d2af19e9be47c65c3fc8889392d22d53a7
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2024-04-19 09:02:14 +0530

    Basic connect disconnect

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-var app = require('express')();
-var http = require('http').Server(app);
+let app = require('express')();
+let http = require('http').Server(app);
 const cors = require('cors');
-var io = require('socket.io')(http, {
+let io = require('socket.io')(http, {
    cors: {
        origin: "http://localhost:3000"
    }
 });
+let clients = [];
 
 app.use(cors());
 
 app.get('/', function(req, res){
+   clients.forEach(function(value) {
+      console.log(value.name)
+   })
    res.json({
       message: 'Hello world',
     });
@@ -18,13 +22,17 @@ app.get('/', function(req, res){
 //Whenever someone connects this gets executed
 io.on('connection', function(socket){
    console.log('A user connected');
-   
-   //Whenever someone disconnects this piece of code executed
-   socket.on('disconnect', function () {
-      console.log('A user disconnected');
+   socket.on('client:join', function (name) {
+      clients.push({name:name,sobj:socket})
    });
-   socket.on('message', function (msg) {
-      console.log(msg);
+   socket.on('disconnect', function () {
+      clients.find(function(obj,index) {
+         if (obj.sobj === socket) {
+            console.log(obj.name+" disconnected")
+            clients.splice(index,1)
+            return true
+         }
+      })
    });
 });
 http.listen(4000, function(){
